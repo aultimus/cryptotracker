6d99649ee08428c4e6aa0e8d4a20705955707a43
Matthew Ault <matthew.ault@segment.com>
2022-04-23 12:17:10 -0400

    retrieve pairs list from cryptowatch

--- cryptotracker/__init__.py
+++ cryptotracker/__init__.py
@@ -1,12 +1,34 @@
-import datetime, functools, os
+import functools, os, requests
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from flask import Flask
 
+# TODO: use a database rather than this global state
+# read/writes to pairs could possibly be unthreadsafe
+pairs = {}
+
 # TODO support multiple simultaneous exchanges
 def fetch_pairs(exchange_name):
-  # TODO: implement
-  print(datetime.datetime.now())
+  # seeing as we are only requesting data once per minute then we do not
+  # need to utilise streaming
+  # TODO: make timeout a config value
+  try:
+   response = requests.get("https://api.cryptowat.ch/markets/{}".format(exchange_name.lower()), timeout=(3,10))
+  except requests.exceptions.RequestException as e:
+    print(e)
+    return
+
+  response_json = response.json()
+  if response.status_code >= 300:
+    print("status:{}".format(response.status_code), "error:",response_json["error"]) # TODO: more sophisticated logging
+    # TODO: more sophisticated retry/backoff policy
+    return
+
+  # collect list of active pairs on this exchange
+  for d in response_json["result"]:
+    if d["active"]:
+      pairs[d["pair"]] = {}
+  print(pairs)
 
 # TODO use production server e.g. waitress
 def create_app(test_config=None):
